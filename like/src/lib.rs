a7ecec41d11bc09dfdbe0d64e746d2ba5bc4a616
Threkork <threkork@hotmail.com>
2024-08-28 17:09:58 +0800

    modified:   Cargo.toml 	modified:   like/Cargo.toml 	modified:   like/src/lib.rs

--- like/src/lib.rs
+++ like/src/lib.rs
@@ -16,6 +16,7 @@ struct Config {
     today: Vec<i64>,
     /// 储存秒级别时间戳
     data_time: u64,
+    like_times: usize,
     msg: Msg,
 }
 
@@ -33,19 +34,31 @@ pub async fn main(mut p: PluginBuilder) {
     path.push("config.json");
 
     let config: Config = {
-        let config = serde_json::from_value(json!({
+        let config: Config = serde_json::from_value(json!({
             "today": [],
             "data_time": 1,
+            "like_times": 10,
             "msg": {
                 "cmd": "赞我",
                 "like": "已为你点赞10次",
                 "today": "今天赞过了，一边呆着去！",
-                "do_not_like_you":"就不给你点，略略略"
+                "do_not_like_you": "就不给你点，略略略"
             }
         }))
         .unwrap();
 
-        let mut config: Config = load_json_data(config, &path).unwrap();
+        let mut config: Config = match load_json_data(config.clone(), &path) {
+            Ok(v) => v,
+            Err(e) => {
+                // 是json解析报错的话?
+                if let Some(_parse_error) = e.downcast_ref::<serde_json::Error>() {
+                    save_json_data(&config, &path).unwrap();
+                    config
+                } else {
+                    panic!("{e}")
+                }
+            }
+        };
         let now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
@@ -73,17 +86,18 @@ pub async fn main(mut p: PluginBuilder) {
             let mut config_lock = config_clone.lock().unwrap();
 
             if config_lock.today.contains(&e.user_id) {
-                e.reply_and_quote(&msg.today)
-            } else {
-                match bot.send_like_return(e.user_id, 10) {
-                    Ok(_) => {
-                        e.reply_and_quote(&msg.like);
-                        config_lock.today.push(e.user_id);
-                        save_json_data(&*config_lock, &path).unwrap();
-                    }
-                    Err(_) => e.reply_and_quote(&msg.do_not_like_you),
-                };
+                e.reply_and_quote(&msg.today);
+                return;
             }
+
+            match bot.send_like_return(e.user_id, 10) {
+                Ok(_) => {
+                    e.reply_and_quote(&msg.like);
+                    config_lock.today.push(e.user_id);
+                    save_json_data(&*config_lock, &path).unwrap();
+                }
+                Err(_) => e.reply_and_quote(&msg.do_not_like_you),
+            };
         }
     });
 
